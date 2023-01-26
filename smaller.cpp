48da1059d201676853aee856aa0dff9ebc127822
JoseG777 <josegc7773@gmail.com>
2023-01-26 18:52:56 -0500

    code for smaller

---
+++ smaller.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+  int num1;
+  int num2;
+  std::cout<< "Input your first integer: \n";
+  std::cin>> num1;
+  std::cout<<  "Input your second integer: \n";
+  std::cin>> num2;
+  if(num1 < num2){
+    std::cout<<"The smaller of the two is "<< num1;
+  }else{
+    std::cout<<"The smaller of the two is "<< num2;
+  }
+  return 0;
+}
